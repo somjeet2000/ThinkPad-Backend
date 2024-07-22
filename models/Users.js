1a4c619376ba5e63b0271f700a6e983921ed1939
Somjeet Srimani <somjeetsrimani2000@gmail.com>
2024-07-23 02:38:07 +0530

    Tried to add the functionality for Forgot password

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
   name: {
@@ -19,6 +21,29 @@ const userSchema = new Schema({
     type: Date,
     default: Date.now, //Donot call the function here
   },
+  // Added for the functionality of Forgot password --START--
+  securityQuestion: {
+    type: String,
+    required: true,
+  },
+  securityAnswer: {
+    type: String,
+    required: true,
+  },
+  // Added for the functionality of Forgot password --END--
+});
+
+// Pre-save hook to hash password and security answer
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  if (this.isModified('securityAnswer')) {
+    const salt = await bcrypt.genSalt(10);
+    this.securityAnswer = await bcrypt.hash(this.securityAnswer, salt);
+  }
+  next();
 });
 
 module.exports = mongoose.model('user', userSchema);
